87b25aa31a7c1099a5d825143c0aa268b20ecf51
J Taylor <taylorj999@gmail.com>
2014-06-16 14:33:04 -0400

    Added ability to set default tags on loading

--- python-imgloader/loader.py
+++ python-imgloader/loader.py
@@ -6,11 +6,22 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import datetime
 import shutil
+import argparse
 
 # directory locations
 input_dir = "C:/Development/images/input"
 thumbnail_dir = "C:/Development/images/thumbnails"
 done_dir = "C:/Development/images/processed"
+
+# parse the command line arguments to see if there are
+# any default tags specified
+parser = argparse.ArgumentParser()
+parser.add_argument('--dt',action='append')
+args = parser.parse_args()
+if args.dt != None:
+	tags = args.dt
+else:
+	tags = []
 
 client = MongoClient()
 db = client.gallery
@@ -26,6 +37,8 @@ for fName in onlyfiles:
      thumbname = "thumbnail-" + imgname
      # check if the destination directories exist
      # create them if they do not
+     # subdirs are used so we're not dumping 1000s of images
+     # in one directory
      if os.access(join(done_dir,str_id[:8]),os.R_OK) == False:
      	os.mkdir(join(done_dir,str_id[:8]))
      if os.access(join(thumbnail_dir,str_id[:8]),os.R_OK) == False:
@@ -49,7 +62,10 @@ for fName in onlyfiles:
 			   "original" : fName,
 			   "new" : True,
 			   "location" : str_id[:8] + "/" + imgname,
-			   "thumbnail" : str_id[:8] + "/" + thumbname}
+			   "thumbnail" : str_id[:8] + "/" + thumbname,
+			   "x" : s1,
+			   "y" : s2,
+			   "tags" : tags}
      coll.insert(imgdoc)
      # and finally move the old file to the new location and name
      shutil.move(join(input_dir,fName),join(done_dir,str_id[:8],imgname))
