2d4449f0eeb73c03faba34381519e4fb0ce17e2b
taylorj999 <taylorj999@gmail.com>
2014-09-10 16:22:19 -0400

    Added reserved (system defined) tag list. These are tags that are defined programatticaly rather than user added.

--- node-mongo-gallery/config/config.js
+++ node-mongo-gallery/config/config.js
@@ -1,15 +1,18 @@
 var config = {};
 
 // system configuration, used to define back end features
-// anything that never is required by the Swig layer goes in here
+// anything that never is required by the display layer goes in here
 config.system = {};
 
 config.system.mongoConnectString = 'mongodb://localhost:27017/gallery';
 config.system.sessionKey = 'insertyoursecrethere';
 config.system.galleryServerPort = 3000;
 
+// reserved tag keywords, things which are inferred and never assigned directly
+config.system.reservedTags = ['new','deleted','untagged'];
+
 // site configuration, used to build pages or allow/disallow actions globally
-// anything that needs to be passed to the Swig layer goes in here
+// anything that needs to be passed to the display layer goes in here
 config.site = {};
 
 // number of thumbnails to show per page
